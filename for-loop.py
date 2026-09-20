c53a4d482a47bfed744ace0d99a81d424e22cbee
Harshita Sharma <harshitapandit1417@gmail.com>
2026-09-20 23:25:25 +0530

    Create for-loop.py

---
+++ for-loop.py
@@ -0,0 +1,10 @@
+#You are given a list of exam scores, and you have to print out the highest score from the List. You will need to use what you have learnt about Lists, For Loops and Conditionals to print out the highest score in the list of student_scores. 
+
+
+student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
+
+max = 0
+for score in student_scores:
+    if score>max:
+        max = score
+print(max)
